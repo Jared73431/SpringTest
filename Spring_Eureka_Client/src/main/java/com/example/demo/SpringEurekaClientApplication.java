de25ecedeaca6f39e4c1aee206e27805b4f55546
Jared <jared@innoagile.com.tw>
2023-01-06 15:05:10 +0800

    spring feign client Update

--- Spring_Eureka_Client/src/main/java/com/example/demo/SpringEurekaClientApplication.java
+++ Spring_Eureka_Client/src/main/java/com/example/demo/SpringEurekaClientApplication.java
@@ -2,10 +2,10 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.example.demo.service"})
+@EnableFeignClients
 public class SpringEurekaClientApplication {
 
 	public static void main(String[] args) {
