f5f7f478d7b896d4fbd13f57abd4e7676e03d3d2
JARED\Jared <jared.831218dragon14@yahoo.com>
2025-04-08 17:41:34 +0800

    HelloBatch with DB

---
+++ Spring_Batch-2/src/main/java/com/example/demo/configuration/HelloBatchConfig.java
@@ -0,0 +1,36 @@
+package com.example.demo.configuration;
+
+import javax.sql.DataSource;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+
+@Configuration
+public class HelloBatchConfig {
+
+    @Bean
+    public DataSourceTransactionManager transactionManager(DataSource dataSource) {
+        return new DataSourceTransactionManager(dataSource);
+    }
+
+    @Bean
+    public Step sampleStep(JobRepository jobRepository, DataSourceTransactionManager transactionManager) {
+        return new StepBuilder("helloStep3", jobRepository).tasklet((contribution, chunkContext) -> {
+            System.out.println("Hello world2!");
+            return RepeatStatus.FINISHED;
+        }, transactionManager).build();
+    }
+
+    @Bean
+    public Job sampleJob(JobRepository jobRepository, Step sampleStep) {
+        return new JobBuilder("HelloJob2", jobRepository).start(sampleStep).build();
+    }
+
+}
