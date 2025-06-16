4c78717befe0cc5bb0dba887fa749f7a656e4a45
JARED\Jared <jared.831218dragon14@yahoo.com>
2025-06-16 16:53:59 +0800

    Code Fix

--- Spring_Websocket3/src/main/java/com/example/demo/config/WebSocketConfig.java
+++ Spring_Websocket3/src/main/java/com/example/demo/config/WebSocketConfig.java
@@ -12,13 +12,17 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
+        // 啟用簡單消息代理，處理 /topic 和 /queue 前綴的消息
         config.enableSimpleBroker("/topic", "/queue");
+        // 設置應用程序目標前綴，客戶端發送消息時使用
         config.setApplicationDestinationPrefixes("/app");
+        // 設置用戶目的地前綴，用於點對點消息
         config.setUserDestinationPrefix("/user");
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
+        // 註冊 STOMP 端點，啟用 SockJS 後備選項
         registry.addEndpoint("/ws").withSockJS();
     }
 }
